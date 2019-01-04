4d9ad74ae804188ac8d9c226207cc54145a825c9
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-01-04 14:14:38 -0500

    Removed unused libraries. Added all dependency licenses.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
 function defaultTask(cb) {
   cb();
 }
 
+function createLicense(cb) {
+  const pkgPath = path.join(__dirname, "package.json");
+  fs.readFile(pkgPath, (err, data) => {
+    if (!err) {
+      const pkgJSON = JSON.parse(data);
+      let license = fs.readFileSync(path.join(__dirname, "LICENSE")) + "\n";
+      for (dep in pkgJSON.dependencies) {
+        const depDir = path.join(__dirname, "node_modules/" + dep);
+        const depFiles = fs.readdirSync(depDir);
+        depFiles.forEach(file => {
+          if (file.toLowerCase().includes("license")) {
+            const depLicense = path.join(depDir, file);
+            const props = fs.lstatSync(depLicense);
+            if (props.isFile()) {
+              console.log(`Found ${dep} LICENSE file (${file})`);
+              license += `\n==== ${dep.toUpperCase()} ====\n`;
+              license += fs.readFileSync(depLicense);
+            }
+          }
+        });
+      }
+      for (dep in pkgJSON.devDependencies) {
+        const depDir = path.join(__dirname, "node_modules/" + dep);
+        const depFiles = fs.readdirSync(depDir);
+        depFiles.forEach(file => {
+          if (file.toLowerCase().includes("license")) {
+            const depLicense = path.join(depDir, file);
+            const props = fs.lstatSync(depLicense);
+            if (props.isFile()) {
+              console.log(`Found ${dep} LICENSE file (${file})`);
+              license += `==== ${dep.toUpperCase()} ====\n`;
+              license += fs.readFileSync(depLicense) + "\n";
+            }
+          }
+        });
+      }
+      const licensePath = path.join(__dirname, "bin" + path.sep + "LICENSES.txt");
+      fs.writeFileSync(licensePath, license);
+    }
+    cb();
+  });
+}
+
 exports.default = defaultTask;
+exports.license = createLicense;
